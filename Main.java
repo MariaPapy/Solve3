bff3aa155129a3047aadc00418f17c1026addf9f
MariaPapy <124443897+MariaPapy@users.noreply.github.com>
2023-02-27 21:03:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        Phone phone = new Phone("+79991112200","iPhone11", 200);
-        phone.toString();
-        phone.receiveCall("Мой номер ");
-        phone.sendMessage();
+        Circle circle = new Circle(300, "pink");
+        circle.toString();
     }
 }
